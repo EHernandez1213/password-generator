0d4e06c1de5a249e6a569e80c7977f980fb90811
Whynotwynaut <125845043+EHernandez1213@users.noreply.github.com>
2023-04-06 21:57:22 -0700

    Added to javascript and readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# password-generator
+# Password-Generator
+
+## Description
+
+Generates a password based on criteria inputed by the user. This password will consist of random characters.
+
+## Installation
+
+N/A
+
+## Usage
+
+When user clicks the generate password button, they will be asked for the criteria. The user will first be asked how many characters the password should have. The user will input the desired length. The user will then be asked if it should include special characters, uppercase letters, lower case letters and numbers. The user can select it or not. If the password length is long enough and at least one of the character types is selected, a password will be generated. If the password length is too short they will be told that the length is too short. if they do not select any of the character types they will be asked to pick one.
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2023 Elenilson Hernandez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,91 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var password = ""
+
+var specialCharcters = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]","{","}","~","`",",",".","<",">","/","?","|"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// arrays created for password components
+
+function getRandomCharacter(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  var randomPasswordCharacter = array[randomIndex];
+  return randomPasswordCharacter;
+}
+// gets a random character from an array
+
+function generatePassword() {
+  var passwordLength = prompt("How many characters should the password be?");
+  var includeSpecialCharacters = confirm("Include special characters?");
+  var includeUpperCase = confirm("Include uppercase letters?");
+  var includeLowerCase = confirm("Include lowercase letters?");
+  var includeNumbers = confirm("Include numbers?");
+// prompts user to give password criteria
+
+  var passwordCriteria = {
+    passwordLength: passwordLength,
+    includeSpecialCharacters: includeSpecialCharacters,
+    includeUpperCase: includeUpperCase,
+    includeLowerCase: includeLowerCase,
+    includeNumbers: includeNumbers,
+  };
+  var possibleCharacters = [];
+  password = [];
+  
+
+  if (passwordLength >= passwordCriteria.length -1) {
+    //checks to see if password length is long enough for criteria
+    if (passwordCriteria.includeSpecialCharacters) {
+      possibleCharacters = possibleCharacters.concat(specialCharcters);
+      var randomCharacter = getRandomCharacter(specialCharcters);
+      password.push(randomCharacter);
+    }
+// inserts a random special character if prompted to include special characters
+
+    if (passwordCriteria.includeUpperCase) {
+      possibleCharacters = possibleCharacters.concat(upperCase);
+      var randomCharacter = getRandomCharacter(upperCase);
+      password.push(randomCharacter);
+    }
+// inserts random uppercase letter if prompted to include uppercase letters
+  
+    if (passwordCriteria.includeLowerCase) {
+      possibleCharacters = possibleCharacters.concat(lowerCase);
+      var randomCharacter = getRandomCharacter(lowerCase);
+      password.push(randomCharacter);
+    }
+// inserts random lowercase letter if promted to include lowercase letters
+
+    if (passwordCriteria.includeNumbers) {
+      possibleCharacters = possibleCharacters.concat(numbers);
+      var randomCharacter = getRandomCharacter(numbers);
+      password.push(randomCharacter);
+    }
+// inserts random number if promted to include numbers
+
+  for (var i = 0; i < password.length - passwordCriteria.length; i++) {
+    var randomCharacter = getRandomCharacter(possibleCharacters);
+    password.push(randomCharacter);
+    }
+
+    password = password.join("");
+
+  }
+  else if (passwordCriteria.length == 1) {
+    password = "Please include a character type";
+    
+  }
+  else {
+    password = "Password length is too short";
+  }
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
